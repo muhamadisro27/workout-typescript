90fc2b3763e7752b27839eb868de315605c11ff1
muhamadisro27 <isro.sabanur@trans-cosmos@co.id>
2024-06-20 11:56:04 +0700

    feat: add schema

---
+++ 5.belajar-typescript-restful-api/prisma/migrations/20240619095121_create_all_tables/migration.sql
@@ -0,0 +1,40 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `username` VARCHAR(100) NOT NULL,
+    `password` VARCHAR(100) NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `token` VARCHAR(100) NULL,
+
+    PRIMARY KEY (`username`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `contacts` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `firstName` VARCHAR(100) NOT NULL,
+    `lastName` VARCHAR(100) NULL,
+    `email` VARCHAR(200) NULL,
+    `phoneNumber` VARCHAR(20) NULL,
+    `username` VARCHAR(100) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `addresses` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `street` VARCHAR(255) NULL,
+    `city` VARCHAR(100) NULL,
+    `province` VARCHAR(100) NULL,
+    `country` VARCHAR(100) NOT NULL,
+    `postalCode` VARCHAR(10) NOT NULL,
+    `contactId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `contacts` ADD CONSTRAINT `contacts_username_fkey` FOREIGN KEY (`username`) REFERENCES `users`(`username`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `addresses` ADD CONSTRAINT `addresses_contactId_fkey` FOREIGN KEY (`contactId`) REFERENCES `contacts`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
